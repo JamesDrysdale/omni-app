95c4cf669afac4416edf8503a92a6ec529d5b43f
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2020-11-05 17:02:00 +0000

    Set up home route

--- App.js
+++ App.js
@@ -9,18 +9,14 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="OMNI Galactic">
+        <Stack.Screen
+          name="OMNI Galactic"
+          component={Home}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
